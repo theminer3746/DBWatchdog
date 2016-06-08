31796279f8de4122ecfb5c26f4bb47baaf254b6d
theminer3746 <bwsminecraft@hotmail.com>
2016-06-08 10:14:54 +0700

    Add service provider

---
+++ DBWatchdog/src/DBWatchdogServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace TriamUdom\DBWatchdog;
+
+use Illuminate\Support\ServiceProvider;
+
+class DBWatchdogServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
